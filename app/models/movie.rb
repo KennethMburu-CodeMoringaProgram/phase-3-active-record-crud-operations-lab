2affe532ac3795ccca4454cc09a05f515ddc00b3
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-02-22 18:06:01 +0300

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,13 @@
 class Movie < ActiveRecord::Base
-
-end
+    def self.released_after(year)
+      where("release_date > ?", year)
+    end
+    def update_title(new_title)
+        self.title = new_title
+        save
+      end
+      def self.delete_all_movies
+        delete_all
+      end
+  end
+  
